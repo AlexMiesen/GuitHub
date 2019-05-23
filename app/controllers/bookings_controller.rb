b7f22bf86d631964fed4d22ef7c337a4317bb13c
Alexander Miesen <alexmiesen@hotmail.com>
2019-05-23 16:40:35 +0200

    the booking now works

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -19,10 +19,19 @@ class BookingsController < ApplicationController
   end
 
   def create
-    @booking = Booking.create(booking_params)
-    @instrument = @booking.instrument
+    @booking = Booking.new(booking_params)
+    @instrument = Instrument.find(params[:instrument_id])
+
+    @booking.instrument = @instrument
+    unlocked = unlock_params
+    startdate = Date.parse("#{unlocked[:booking]["startdate(1i)"]}-#{unlocked[:booking]["startdate(2i)"]}-#{unlocked[:booking]["startdate(3i)"]}")
+    enddate = Date.parse("#{unlocked[:booking]["enddate(1i)"]}-#{unlocked[:booking]["enddate(2i)"]}-#{unlocked[:booking]["enddate(3i)"]}")
+    @booking.no_days = enddate.ld - startdate.ld
+    @booking.total_price = @booking.no_days * @instrument.price_per_day
+    @booking.user = current_user
+
     if @booking.save
-      render "dashboard"
+      redirect_to dashboard_path
     else
       render "instruments/show"
     end
@@ -35,6 +44,12 @@ class BookingsController < ApplicationController
   end
 
   def booking_params
-    params.require(:booking).permit(:instrument, :user, :total_price)
+    params.require(:booking).permit(:instrument, :user, :total_price, :enddate, :startdate)
+  end
+
+  def unlock_params
+    params.permit(:instrument, :user, :total_price, :enddate, :startdate, booking: {})
   end
 end
+
+
