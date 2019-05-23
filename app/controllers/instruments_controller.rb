86cf9b34587776444dc98fd11b07d35dc3a25940
Astrid Le Quere <astrid.lequere@gmail.com>
2019-05-23 11:29:07 +0200

    edit functionality for instrument working without signed in logic

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -1,7 +1,9 @@
 class InstrumentsController < ApplicationController
+  before_action :set_instruments, only: [:show, :edit, :update]
+
   def show
-    @instrument = Instrument.find(params[:id])
   end
+
   def index
     @instruments = Instrument.all
     if params[:instruments].present?
@@ -13,5 +15,28 @@ class InstrumentsController < ApplicationController
         @instruments = @instruments.where(category: params[:instruments][:category])
       end
     end
+  end
+
+  def edit
+  end
+
+  def update
+    @instrument.update(instruments_params)
+
+    if @instrument.update(instruments_params)
+      redirect_to dashboard_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def instruments_params
+    params.require(:instrument).permit(:category, :name, :description, :location, :photo, :price_per_day)
+  end
+
+  def set_instruments
+    @instrument = Instrument.find(params[:id])
   end
 end
