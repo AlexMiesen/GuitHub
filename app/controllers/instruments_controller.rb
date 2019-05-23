27edf625fbd79b228409a16935caacdfd328dd69
Axel Kalland <axelkalland@gmail.com>
2019-05-23 13:32:29 +0200

    added searchbar

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -4,7 +4,11 @@ class InstrumentsController < ApplicationController
   end
   def index
     @instruments = Instrument.all
-    if params[:instruments].present?
+    if params[:query].present?
+      sql_query = "location ILIKE :query OR category ILIKE :query OR description ILIKE :query OR name ILIKE :query"
+      @instruments = Instrument.where(sql_query, query: "%#{params[:query]}%")
+
+    elsif params[:instruments].present?
       if params[:instruments][:location].present?
         @instruments = @instruments.where(location: params[:instruments][:location])
       end
