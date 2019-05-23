73674c2b5e1d991934def9790388da0152065b70
Astrid Le Quere <astrid.lequere@gmail.com>
2019-05-23 12:17:43 +0200

    edit functionality for instrument done without design

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -1,4 +1,6 @@
 class InstrumentsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:show, :index]
+
   before_action :set_instruments, only: [:show, :edit, :update]
 
   def show
